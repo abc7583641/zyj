ce83c09183158b82c6a30bffb50a4f451e3346a4
zyj <abc7583641@163.com>
2019-08-27 08:48:59 +0800

    3rd commit

---
+++ hello_world.py
@@ -0,0 +1,32 @@
+msg="  hello world  "
+msg1="DDDDD"
+msg3=msg+msg1
+#print(msg.rstrip()+"\n"+msg.lstrip()+"\n"+msg.strip());
+#print("ddddd"+str(5*2)+"AAAAAA")	#3633333
+#import this
+bike=['trek','cann','redl','spe','redl']
+bike.reverse()
+dim=(7,6,2)
+#print(dim)
+'''for b in bike:
+	if b=='cann':
+		print(b.upper())
+	else:
+		print(b.title()) '''
+#print('trer' not in bike)
+#bike.sort(reverse=False)
+'''for b in bike:
+	print(b)
+	i=i+1
+	'''
+numbers=list(range(0,13,3))
+copynumers=numbers[:]
+#numbers.append(8)
+#print(copynumers)
+i=17
+if i<4:
+	print("you are lower than 4")
+elif i<=18:
+	print("you are young")
+else:
+	print("you are old enough")
